3f460e1a5592cd7955607b4a1830e8d390bbb2dd
GameOveer <GameOveer@users.noreply.github.com>
2017-01-17 13:39:40 +0200

    Add files via upload
    Added everything

---
+++ bot.py
@@ -0,0 +1,51 @@
+from discord.ext import commands
+import discord
+from cogs.utils import checks
+import datetime, re
+import json, asyncio
+import copy
+import logging
+import traceback
+import sys
+
+initial_extensions = [
+    'cogs.fun',
+    'cogs.debug',
+]
+
+discord_logger = logging.getLogger('discord')
+discord_logger.setLevel(logging.CRITICAL)
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='w')
+log.addHandler(handler)
+
+bot = commands.Bot(command_prefix=['pussymagnet ', '!!'])
+
+@bot.event
+async def on_ready():
+    print('Logged in as:')
+    print('Username: ' + bot.user.name)
+    print('ID: ' + bot.user.id)
+    print('------')
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    await bot.process_commands(message)
+    
+def load_credentials():
+    with open('credentials.json') as f:
+        return json.load(f)
+
+credentials = load_credentials()
+
+if __name__ == '__main__':
+  for extension in initial_extensions:
+    try:
+      bot.load_extension(extension)
+    except Exception as e:
+      print('Failed to load extension {}\n{}: {}'.format(extension, type(e).__name__, e))
+  bot.run(credentials['token'])
