10e1e2009bcb6b017a7352726b3fbcc75bf5a349
GameOveer <GameOveer@users.noreply.github.com>
2017-01-17 13:55:24 +0200

    Create fun category

---
+++ cogs/fun.py
@@ -0,0 +1,17 @@
+import asyncio, time, discord
+from discord.ext import commands
+from .utils import checks
+
+class Fun:
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command(name='ping', pass_context=True, hidden=True)
+	async def ping(self, ctx):
+		before = time.time() * 1000
+    pingmsg = await self.bot.say('i am gay')
+    after = time.time() * 1000
+    await self.bot.edit_message(pingmsg, '**lol {}ms**'.format(round(after - before)))
+
+def setup(bot):
+	bot.add_cog(Fun(bot))
