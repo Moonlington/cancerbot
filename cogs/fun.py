ec664e4572791e152317ae5a35cf3aadecd7ec2b
ofx <ofx@twkly.ml>
2017-01-17 09:38:05 -0500

    admin.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,9 +9,9 @@ class Fun:
 	@commands.command(name='ping', pass_context=True, hidden=True)
 	async def ping(self, ctx):
 		before = time.time() * 1000
-    pingmsg = await self.bot.say('i am gay')
-    after = time.time() * 1000
-    await self.bot.edit_message(pingmsg, '**lol {}ms**'.format(round(after - before)))
+		pingmsg = await self.bot.say('i am gay')
+		after = time.time() * 1000
+		await self.bot.edit_message(pingmsg, '_lol_ **{}ms**'.format(round(after - before)))
 
 def setup(bot):
 	bot.add_cog(Fun(bot))
