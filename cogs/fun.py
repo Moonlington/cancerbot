25592f99fb310507d24e4e858d6b59bbc99e40b4
GameOveer <GameOveer@users.noreply.github.com>
2017-01-18 20:00:05 +0200

    Add more commands to the fun category

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,4 +1,7 @@
 import asyncio, time, discord
+import requests
+import json
+from geopy.geocoders import Nominatim
 from discord.ext import commands
 from .utils import checks
 
@@ -6,12 +9,30 @@ class Fun:
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command(name='ping', pass_context=True, hidden=True)
+	@commands.command(name='ping', pass_context=True)
 	async def ping(self, ctx):
 		before = time.time() * 1000
 		pingmsg = await self.bot.say('i am gay')
 		after = time.time() * 1000
 		await self.bot.edit_message(pingmsg, '_lol_ **{}ms**'.format(round(after - before)))
+
+	@commands.command(name='weather', pass_context=True)
+	async def weather(self, ctx, location:str):
+		geolocator = Nominatim()
+		location = geolocator.geocode(location)
+		geo = geolocator.geocode(location)
+		coordinates = '{},{}'.format(location.latitude, location.longitude)
+		r = requests.get('https://api.darksky.net/forecast/removed/{}'.format(coordinates))
+		jsondata = json.loads(r.text)
+		data = '''
+		:eggplant: :sweat_drops: {} (this is the fucking location you submitted edgy cunt)
+		:eggplant: :sweat_drops: {} (wait, you don't know your timezone, then fucking read this you ignorant pussy)
+		:eggplant: :sweat_drops: {} (kill yourself cunt, if you don't even know what this is already kill yourself none cares)
+		:eggplant: :sweat_drops: {}° (none cares about this anyways smh)
+		:eggplant: :sweat_drops: {}° (none cares about this anyways smh)
+		'''.format(location.address, jsondata['timezone'], jsondata['currently']['summary'], round(jsondata['currently']['temperature']), round((int(jsondata['currently']['temperature'])-32)*5.0/9))
+		msg = await self.bot.say('i am gay')
+		await self.bot.edit_message(msg, '*_lol done you fuckin cunt, fucking kill yourself you fucking edgy fucking fucking fuckwad you fucking niggerfaggot_*\n{}'.format(data))
 
 def setup(bot):
 	bot.add_cog(Fun(bot))
